a0a513d730d6ac003c8d61dfe76653a04d070da6
Luis <carneiroluis2@gmail.com>
2023-08-21 01:03:39 -0400

    ...

--- src/constants/Data.js
+++ src/constants/Data.js
@@ -1,6 +1,6 @@
 export const DIGITALOCEAN = 'https://vemitienda.nyc3.digitaloceanspaces.com/'
 // export const URL_PRODUCTION = 'https://vemitienda.online/api/v1/'
-export const URL_PRODUCTION = 'https://vemitienda.online/api/v2/'
+export const URL_PRODUCTION = 'https://vemitienda.online/api/v3/'
 export const URL_CATALOG = 'https://vemitienda.online/'
 export const URL_BASE = 'https://vemitienda.online/'
 export const COUNTRY_ID = 1
