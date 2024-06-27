81d61ea1999698c5b59dae04e36d968db35ac743
luiscarneiro13 <carneiroluis2@gmail.com>
2024-06-27 11:41:30 -0400

    ...

--- src/constants/Data.js
+++ src/constants/Data.js
@@ -1,7 +1,7 @@
-export const DIGITALOCEAN = 'https://vemitienda.com/'
+export const DIGITALOCEAN = 'https://vemitienda.com.ve/'
 // export const URL_PRODUCTION = 'https://vemitienda.online/api/v1/'
-export const URL_PRODUCTION = 'https://vemitienda.com/api/v3/'
-export const URL_CATALOG = 'https://vemitienda.com/'
-export const URL_BASE = 'https://vemitienda.com/'
+export const URL_PRODUCTION = 'https://vemitienda.com.ve/api/v3/'
+export const URL_CATALOG = 'https://vemitienda.com.ve/'
+export const URL_BASE = 'https://vemitienda.com.ve/'
 export const COUNTRY_ID = 1
-export const VERSION = '1.3.5'
+export const VERSION = '1.3.6'
