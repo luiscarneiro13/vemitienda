d53a67768a0bcd989ecab63fb727983465efee3f
luiscarneiro13 <carneiroluis2@gmail.com>
2025-08-26 12:16:29 -0400

    ...

--- src/constants/Data.js
+++ src/constants/Data.js
@@ -1,3 +1,7 @@
+import Constants from 'expo-constants'
+
+const appVersion = Constants.expoConfig.version
+
 const DOMINIO = 'https://vemitienda.com.ve/'
 
 export const DIGITALOCEAN = DOMINIO
@@ -5,4 +9,4 @@ export const URL_PRODUCTION = DOMINIO + 'api/v3/'
 export const URL_CATALOG = DOMINIO
 export const URL_BASE = DOMINIO
 export const COUNTRY_ID = 1
-export const VERSION = '1.5.1'
+export const VERSION = appVersion
